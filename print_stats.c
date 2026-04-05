c545b795c16a0da0a9006a000d881e592f14e821
Davide Angelocola <davide.angelocola@gmail.com>
2026-04-05 19:45:46 +0200

    build: drop scripts created by AI and commited by mistake

--- print_stats.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <rocksdb/statistics.h>
-
-int main() {
-    printf("NUMBER_KEYS_WRITTEN: %d\n", rocksdb::NUMBER_KEYS_WRITTEN);
-    printf("NUMBER_KEYS_READ: %d\n", rocksdb::NUMBER_KEYS_READ);
-    printf("DB_GET: %d\n", rocksdb::DB_GET);
-    return 0;
-}
